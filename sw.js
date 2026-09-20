ee151fe06641d08510b19b51a16c481d168639bb
morblond <53000574+morblond@users.noreply.github.com>
2026-09-20 11:15:11 +0200

    fix: refresh PWA cache after automatic import update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'carbuvision-v4';
+const CACHE = 'carbuvision-v5';
 const APP_SHELL = ['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('carbuvision-') && key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
